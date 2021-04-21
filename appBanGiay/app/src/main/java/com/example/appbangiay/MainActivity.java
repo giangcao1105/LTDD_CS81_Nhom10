4b1b3e856263c6f3765bd3a34f49e3e6064788c1
z@n9_k@0 <39848355+giangcao1105@users.noreply.github.com>
2021-04-21 22:46:40 +0700

    form dang nhap & dang ky

--- appBanGiay/app/src/main/java/com/example/appbangiay/MainActivity.java
+++ appBanGiay/app/src/main/java/com/example/appbangiay/MainActivity.java
@@ -2,13 +2,32 @@ package com.example.appbangiay;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button bt_register;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        bt_register = findViewById(R.id.btn_register);
+
+        bt_register.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openFormReg();
+            }
+        });
+    }
+
+    private void openFormReg() {
+        Intent intent = new Intent(this, register_form.class);
+        startActivity(intent);
     }
 }
