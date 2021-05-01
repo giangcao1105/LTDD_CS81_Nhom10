e9a01639e528612d1ebc2b2d1de4682de083a4dc
Riri <vohuy9112000@gmail.com>
2021-05-02 00:04:35 +0700

    fix bug k ton tai

--- appBanGiay/app/src/main/java/com/example/appbangiay/ui/carts/CartFragment.java
+++ appBanGiay/app/src/main/java/com/example/appbangiay/ui/carts/CartFragment.java
@@ -19,6 +19,6 @@ public class CartFragment extends Fragment {
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_cart, container, false);
+        return inflater.inflate(R.layout.activity_main, container, false);
     }
 }
