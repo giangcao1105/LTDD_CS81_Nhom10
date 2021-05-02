3685be707a8a9c4a3ed7f906ea196676d650282a
khoado <do.khoa.18400@gmail.com>
2021-05-02 18:02:42 +0700

    fix homepage++

--- appBanGiay/app/src/main/java/com/example/appbangiay/homepage_form.java
+++ appBanGiay/app/src/main/java/com/example/appbangiay/homepage_form.java
@@ -7,6 +7,10 @@ import android.view.MenuItem;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.AppBarConfiguration;
+import androidx.navigation.ui.NavigationUI;
 
 import com.example.appbangiay.ui.carts.CartFragment;
 import com.example.appbangiay.ui.home.HomeFragment;
@@ -23,7 +27,14 @@ public class homepage_form extends AppCompatActivity {
         BottomNavigationView navView = findViewById(R.id.nav_view);
         navView.setOnNavigationItemSelectedListener(navListener);
 
-        getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new HomeFragment()).commit();
+//        getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new HomeFragment()).commit();
+
+        AppBarConfiguration appBarConfiguration = new AppBarConfiguration.Builder(
+                R.id.navigation_home, R.id.navigation_user, R.id.navigation_cart)
+                .build();
+        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
+        NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
+        NavigationUI.setupWithNavController(navView, navController);
     }
 
     private final BottomNavigationView.OnNavigationItemSelectedListener navListener = new BottomNavigationView.OnNavigationItemSelectedListener() {
@@ -33,13 +44,13 @@ public class homepage_form extends AppCompatActivity {
             Fragment selectedFragment = null;
 
             switch (item.getItemId()) {
-                case R.id.navi_home:
+                case R.id.navigation_home:
                     selectedFragment = new HomeFragment();
                     break;
-                case R.id.navi_user:
+                case R.id.navigation_user:
                     selectedFragment = new UserFragment();
                     break;
-                case R.id.navi_cart:
+                case R.id.navigation_cart:
                     selectedFragment = new CartFragment();
                     break;
             }
